9a343a8d6cce85d294f8149bf9214d9bc2a4a761
Sarabjeet Singh <sarab@Sarabjeets-MBP.lan>
2023-10-20 21:31:32 -0400

    algorithm changes

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/sarab/Documents/UDM/Hackarthon/env"
+set -gx VIRTUAL_ENV "/Users/sarab/Documents/UDM/Hackarthon/hackarthon-Detroit/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
